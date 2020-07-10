8b942f36a939055a6f11cbd258e3aa06396e99fc
Steven Topazio <sat@northpointprinting.com>
2020-07-10 18:02:53 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,17 +1,1 @@
-require_relative
-
-class Array
-  def my_each
-    if block_given?
-      i = self.count-1
-      n = 0
-      while n <= i
-        yield(self[n])
-        n += 1
-      end
-    else return self
-  end
-end
-
-array = [1,2,3,4,5,6]
-array.my_each { |element| puts element + 1 }
+def
